2bd51b342c4a1f2bb6e6677bf4c2bec9ead996df
Christian Montoya <chrimonlo@gmail.com>
2022-03-09 15:34:13 -0500

    Collitions added, points working and the snake body increase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Creation of board of the game
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreBoard import ScoreBoard
 import time
 #Creation of the board
 screen = Screen()
@@ -8,13 +10,48 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Programate snake game")
 screen.tracer(0)
+#Creation of the Snake and the Food
 snake = Snake()
+food = Food()
+scoreBoard = ScoreBoard()
+#Listen keys method
+def movement(method,direction):
+    screen.listen()
+    screen.onkey(method, direction)
+
+def finish_game():
+    game_is_on = False
+    scoreBoard.game_over()
+movement(snake.up, "Up")
+movement(snake.left, "Left")
+movement(snake.down, "Down")
+movement(snake.right, "Right")
+
 #Snake animation
 game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
+    time.sleep(0.1)
     snake.move()
 
+    #Detect collition with the food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreBoard.increase_points();
+        snake.extend()
+
+    #Detect collition with the board
+    if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreBoard.game_over()
+
+
+    #Detect collition with the body of the snake
+    for i in snake.snake_body:
+        if i == snake.head:
+            pass
+        elif snake.head.distance(i)<10:
+            game_is_on = False
+            scoreBoard.game_over()
 screen.exitonclick()
---
+++ scoreBoard.py
@@ -0,0 +1,26 @@
+
+from turtle import Turtle
+class ScoreBoard(Turtle):
+    FONT = ('Verdana', 30, 'bold')
+    ALIGN = 'left'
+    points = 0
+    def __init__ (self):
+        
+        super().__init__()
+        self.goto((-295,260))
+        self.showPoints()
+
+    #Method to draw the points using turtle.write
+    def showPoints (self):
+        self.color("green")
+        self.write(f"POINTS: {self.points}", False, align=self.ALIGN, font = self.FONT)
+        self.hideturtle()
+
+    def increase_points(self):
+        self.clear()
+        self.points += 1
+        self.showPoints()
+    
+    def game_over(self):
+        self.clear()
+        self.write("GAME OVER", False, align=self.ALIGN, font = self.FONT)
